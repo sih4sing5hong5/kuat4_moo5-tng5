d7bd9ea7d73ff17a282c8768f156a7e7ad854c98
Ihc <Ihc@gmail.com>
2014-03-25 21:43:18 +0800

    加管理介面　丞宏

---
+++ 選區資料/管理.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from 選區資料.模型 import 候選人
+from 選區資料.模型 import 投開票所
+from 選區資料.模型 import 選區
+admin.site.register(選區)
+admin.site.register(投開票所)
+admin.site.register(候選人)
