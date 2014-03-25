5fbaaeeab752e03e94f306f504c5097ff61598f5
Ihc <Ihc@gmail.com>
2014-03-25 22:36:25 +0800

    加看選區資料　丞宏

--- 選區資料/介面.py
+++ 選區資料/介面.py
@@ -11,10 +11,18 @@ from django.utils.decorators import method_decorator
 from django.db.models import Q
 from 選區資料.模型 import 選區
 
-def 看選區(request):
+def 看全部選區(request):
 	全部選區 = 選區.objects.all()
 	版 = loader.get_template('選區資料/全部選區.html')
 	文 = RequestContext(request, {
 		'全部選區': 全部選區,
+	})
+	return HttpResponse(版.render(文))
+
+def 看選區(request,pk):
+	選區資料 = 選區.objects.get(pk=pk)
+	版 = loader.get_template('選區資料/選區.html')
+	文 = RequestContext(request, {
+		'選區': 選區資料,
 	})
 	return HttpResponse(版.render(文))
--- 選區資料/網址.py
+++ 選區資料/網址.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 from 選區資料.介面 import 看選區
+from 選區資料.介面 import 看全部選區
 
 
 urlpatterns = patterns('',
-	url(r'^.*$', 看選區, name = '看選區'),
-	url(r'^看選區$', 看選區, name = '看選區'),
+	url(r'^看選區/(?P<pk>\d+)$', 看選區, name = '看選區'),
+	url(r'^.*$', 看全部選區, name = '看全部選區'),
 )
