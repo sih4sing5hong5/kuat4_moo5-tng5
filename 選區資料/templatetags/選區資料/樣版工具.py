c3fcc652b20d31a7bab67c79d8b094a19c94264f
Ihc <ihcaoe@gmail.com>
2014-03-26 18:15:22 +0800

    整理表 丞宏

---
+++ 選區資料/templatetags/選區資料/樣版工具.py
@@ -0,0 +1,34 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def 轉置( 矩陣 ):
+	"""
+		Filter - returns a list containing range made from given value
+		Usage (in template):
+
+		<ul>{% for i in [[ 1, 2, 3],[ 4, 5, 6]]|轉置 %}
+				{% for j in i %}
+					<li>{{ j }}. Do something</li>
+			{% endfor %}
+		{% endfor %}</ul>
+
+		Results with the HTML:
+		<ul>
+			<li>1. Do something</li>
+			<li>4. Do something</li>
+			<li>2. Do something</li>
+			<li>5. Do something</li>
+			<li>3. Do something</li>
+			<li>6. Do something</li>
+		</ul>
+	"""
+	return zip(*矩陣)
+	
+@register.filter
+def 變字串(物件):
+	if isinstance(物件, float):
+		return round(物件,1)
+	return 物件
+		
