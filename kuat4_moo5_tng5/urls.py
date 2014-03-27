8ecc73fe942ec5bce21655a998e3498cb077fd40
Cangming <Cangming@Cangming-PC>
2014-03-27 18:01:43 +0800

    首頁更改為罷免清單

--- kuat4_moo5_tng5/urls.py
+++ kuat4_moo5_tng5/urls.py
@@ -5,6 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'罷免/', include('罷免進度.網址')),
-	url(r'^', include('選區資料.網址')),
+	url(r'^看選區', include('選區資料.網址')),
+	url(r'^', include('罷免進度.網址')),
 )
