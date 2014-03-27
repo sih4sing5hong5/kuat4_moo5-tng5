232e2dea8aeb5555fffc88dbd06982ea6a27c165
chhsueh <ihcaod@gmail.com>
2014-03-27 10:52:07 +0800

    改模型 丞宏

--- 罷免進度/模型.py
+++ 罷免進度/模型.py
@@ -7,8 +7,8 @@ class 罷免(models.Model):
 	選區 = models.ForeignKey(選區, related_name='投開票所')
 	開始罷免時間 = models.DateField()
 	結束罷免時間 = models.DateField()
-	有效罷免文件 = models.IntegerField()
-	無效罷免文件 = models.IntegerField()
+	有效罷免文件 = models.IntegerField(default=0)
+	無效罷免文件 = models.IntegerField(default=0)
 	收錄時間 = models.DateTimeField(auto_now_add=True)
 	修改時間 = models.DateTimeField(auto_now=True)
 	def 當選人(self):
