375041961d4d080d6483982ab68575baea6e0629
Ihc <Ihc@gmail.com>
2014-04-04 08:56:21 +0800

    加看選區的json　丞宏

--- 選區資料/介面.py
+++ 選區資料/介面.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.db.models import Q
 from 選區資料.模型 import 選區
+import json
 
 def 看全部選區(request):
 	全部選區 = 選區.objects.all()
@@ -25,7 +26,8 @@ def 看選區json(request):
 	資料=[]
 	for 一个選區 in 全部選區:
 		資料.append((一个選區.縣市,一个選區.第幾區,一个選區.選區總票數()))
-	return HttpResponse(資料)
+# 	return HttpResponse(json.dumps(資料)+'\n'+str(資料))
+	return HttpResponse(str(資料))
 
 def 看選區(request,pk):
 	選區資料 = 選區.objects.get(pk=pk)
